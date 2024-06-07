808a7b99aa79c4cbcf27550c7f650d0a4b75c0fa
tunalee <dongwon1103@naver.com>
2024-06-08 01:39:55 +0900

    [modify] Cors 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -393,8 +393,9 @@ REST_FRAMEWORK = {
 
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = default_methods
+CORS_ALLOWED_ORIGINS = ["http://localhost:8000", "https://han.glint.team"]
 
 # Your stuff...
 # ------------------------------------------------------------------------------
